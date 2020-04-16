c299bea8d325418877d2ddf575d61020deabb104
Chad Hinds <chadrickj8@gmail.com>
2020-04-16 16:48:05 -0400

    Create first function

---
+++ game.js
@@ -0,0 +1,20 @@
+// Generate
+function nextSequence() {
+    let randomNumber = Math.ceil(Math.random() * 3);
+    return randomNumber
+}
+
+console.log(nextSequence())
+
+
+
+
+
+
+
+
+
+
+
+
+// alert('OKAY!')
