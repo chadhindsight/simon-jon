237d4e7870ceec19d69bc32a38d0c2cd977cef21
Chad Hinds <chadrickj8@gmail.com>
2020-04-16 16:54:03 -0400

    Store the colors in an array and randomly select one

--- game.js
+++ game.js
@@ -1,7 +1,13 @@
+// Store the colors in an array and randomly select one
+const buttonColors = ["blue", "green", "yellow", "red"];
+let gamePattern = []
+
 // Generate
 function nextSequence() {
     let randomNumber = Math.ceil(Math.random() * 3);
     return randomNumber
+
+    let randomChosenColor = buttonColors[randomNumber];
 }
 
 console.log(nextSequence())
