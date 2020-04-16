5b4dfdd036e1ffc1f708eff703958ab9ea6e7c68
Chad Hinds <chadrickj8@gmail.com>
2020-04-16 18:04:06 -0400

    Upload styles, sounds, etc

--- game.js
+++ game.js
@@ -4,6 +4,20 @@ var buttonColors = ["red", "blue", "green", "yellow"];
 var gamePattern = [];
 var userClickedPattern = [];
 
+// Keep track of whether the game has started or not
+let started = false
+let level = 0;
+// 7th iteration
+$(document).keypress(function () {
+    if (!started) {
+
+        //3. The h1 title starts out saying "Press A Key to Start", when the game has started, change this to say "Level 0".
+        $("#level-title").text("Level " + level);
+        nextSequence();
+        started = true;
+    }
+});
+
 $(".btn").click(function () {
 
     var userChosenColor = $(this).attr("id");
@@ -11,6 +25,7 @@ $(".btn").click(function () {
 
     //When a user clicks on a button, the corresponding sound should be played.
     playSound(userChosenColor);
+    animatePress(userChosenColor);
 
 });
 
@@ -23,20 +38,27 @@ function nextSequence() {
     $("#" + randomChosenColor).fadeIn(100).fadeOut(100).fadeIn(100);
 
     //4. Refactor the code in playSound() so that it will work for both playing sound in nextSequence() and when the user clicks a button.
-    playSound(randomChosenColor);
+    $('button').click(() => playSound(randomChosenColor));
 }
 
 //2. Create a new function called playSound() that takes a single input parameter called name.
 function playSound(name) {
-
-    //3. Take the code we used to play sound in the nextSequence() function and add it to playSound().
-    var audio = new Audio("sounds/" + name + ".mp3");
+    var audio = new Audio(`sounds/${name}.mp3`);
     audio.play();
 }
+// function animatePress(currentColor) {
+//     $(`#${currentColor}`).addClass("pressed")
+// }
+function animatePress(currentColor) {
 
+    //2. Use jQuery to add this pressed class to the button that gets clicked inside animatePress().
+    $(`#${currentColor}`).addClass("pressed");
 
-console.log(nextSequence())
-
+    //3. use Google/Stackoverflow to figure out how you can use Javascript to remove the pressed class after a 100 milliseconds.
+    setTimeout(function () {
+        $(`#${currentColor}`).removeClass("pressed");
+    }, 100);
+}
 
 
 
